b677fcb238b1d8373c77d993fa06e3ca6b45a5f0
Tim Toohey <ttoohey@users.noreply.github.com>
2017-05-24 10:39:41 +1000

    add ability to set per-attribute validation messages in mutations

--- src/Folklore/GraphQL/Support/Traits/ShouldValidate.php
+++ src/Folklore/GraphQL/Support/Traits/ShouldValidate.php
@@ -40,10 +40,44 @@ trait ShouldValidate
         return array_merge($rules ? $rules:$methodRules, $argsRules);
     }
 
-    protected function getValidator($args, $rules)
+    protected function getValidator($args, $rules, $messages)
     {
-        return app('validator')->make($args, $rules);
+        return app('validator')->make($args, $rules, $messages);
     }
+
+    protected function setMessages($messages)
+    {
+        $this->attributes['messages'] = $messages;
+        return $this;
+    }
+    
+    public function getMessages()
+    {
+        return array_get($this->attributes, 'messages');
+    }
+    
+    protected function getMessagesForValidator()
+    {
+        $arguments = func_get_args();
+        
+        $messages = array_get($this->attributes, 'messages');
+        $methodMessages = method_exists($this, 'messages') ?
+            call_user_func_array([$this, 'messages'], $arguments):[];
+        $argsMessages = [];
+        $args = $this->getArgs();
+        foreach ($args as $name => $arg) {
+            if (isset($arg['messages'])) {
+                if ($arg['messages'] instanceof Closure) {
+                    $argsMessages[$name] = call_user_func_array($arg['messages'], $arguments);
+                } else {
+                    $argsMessages[$name] = $arg['messages'];
+                }
+            }
+        }
+
+        return array_merge($messages ? $messages:$methodMessages, $argsMessages);
+    }
+
 
     public function getResolver()
     {
@@ -56,9 +90,10 @@ trait ShouldValidate
             $arguments = func_get_args();
 
             $rules = call_user_func_array([$this, 'getRulesForValidator'], $arguments);
+            $messages = call_user_func_array([$this, 'getMessagesForValidator'], $arguments);
             if (sizeof($rules)) {
                 $args = array_get($arguments, 1, []);
-                $validator = $this->getValidator($args, $rules);
+                $validator = $this->getValidator($args, $rules, $messages);
                 if ($validator->fails()) {
                     throw with(new ValidationError('validation'))->setValidator($validator);
                 }
